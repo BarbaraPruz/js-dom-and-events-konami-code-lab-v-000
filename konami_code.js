2bea2ee49d0b4348a6ea1aa9cdfb9ab51894e5eb
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-09 00:53:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const body = document.getElementById('body');
+  let codeIdx = 0;
+  body.addEventListener('keydown', function(event) {
+    console.log("event ",event.which,"type",typeof(event.which));
+    if (event.which === code[codeIdx]) {
+      if (++codeIdx >= code.length)
+          alert('Congratulations!');
+      }
+      codeIdx = 0;
+    }
+  });
 }
