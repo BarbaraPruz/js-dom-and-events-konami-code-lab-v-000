dc3c0e5c5d5f0fd4bd86186a9df5ff8ce6fe3dfc
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-09 01:15:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let codeIdx = 0;
+  document.querySelector('body').addEventListener('keydown', function(event) {
+    const key = parseInt(event.detail || event.which);
+    if (key == code[codeIdx]) {
+      if (++codeIdx >= code.length) {
+          alert('Congratulations!');
+          codeIdx = 0;
+      }
+    }
+    else {
+      codeIdx = 0;
+    }
+  });
 }
